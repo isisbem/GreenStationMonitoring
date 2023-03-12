49ebe8f3d8d2cc340f4d5ac7bf79289240ae1370
Matty <74145047+Matteo0099@users.noreply.github.com>
2023-03-12 15:40:56 +0100

    Some updates

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,38 +3,38 @@
   <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-green-500 mb-3">
     <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
       <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
-        <Router-Link class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+        <Router-Link class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white outline-none focus:outline-none active:outline-1 active:outline active:outline-slate-300 active:rounded p-3"
           to="/">
           Logo
         </Router-Link>
         <button
-          class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+          class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none active:outline-1 active:outline active:outline-slate-300"
           type="button" v-on:click="toggleNavbar()">
           <i class="fas fa-bars"></i>
         </button>
       </div>
-      <div v-bind:class="{ 'hidden': !showMenu, 'flex': showMenu }" class="lg:flex lg:flex-grow items-center">
-        <ul class="flex flex-col lg:flex-row list-none ml-auto">
+      <div v-bind:class="{ 'hidden': !showMenu, 'flex': showMenu }" class="lg:flex flex-grow lg:flex-grow-0 w-full lg:w-fit md:justify-end md:items-end justify-center items-center transition-all">
+        <ul class="flex flex-col lg:flex-row list-none mx-auto">
           <li class="nav-item">
-            <RouterLink class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+            <RouterLink class="px-3 py-2 flex my-1 justify-center items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
               to="/">
               <span class="ml-2">Home</span>
             </RouterLink>
           </li>
           <li class="nav-item">
-            <RouterLink class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+            <RouterLink class="px-3 py-2 flex my-1 justify-center items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
               to="/About">
               <span class="ml-2">About</span>
             </RouterLink>
           </li>
           <li class="nav-item">
-            <RouterLink class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+            <RouterLink class="px-3 py-2 flex my-1 justify-center items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
               to="/Analytics">
               <span class="ml-2">Analytics</span>
             </RouterLink>
           </li>
           <li class="nav-item">
-            <RouterLink class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+            <RouterLink class="px-3 py-2 flex my-1 justify-center items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
               to="/Informations">
               <span class="ml-2">Informations</span>
             </RouterLink>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,23 @@
 <template>
     <div>
         <div class="about h-full">
-            <div class="h-2/3 container mt-4 flex justify-center items-center text-black">
-                <h1>about</h1>
+            <div class="h-2/3 mt-4 flex justify-center items-center text-black">
+                <div class="flex flex-col items-center justify-center w-full max-w-5xl mt-16 mx-auto">
+                    <!-- title -->
+                    <h1 class="font-semibold">About us</h1>
+                    <!-- table with all items -->
+                    <div class="grid grid-flow-col gap-3 w-full mt-4">
+                        <!-- 1 row -->
+                        <div class="bg-blue-100 col-span-1">
+                            Title
+                        </div>
+                        <div class="bg-red-100 col-span-4">
+                            Content                            
+                        </div>
+                        <!-- 2 row -->
+                        <!-- 3 row -->
+                    </div>
+                </div>
             </div>
         </div>
     </div>
