cdd5011a3504822cd5ea28e4e81878e21a7d5433
Matteo0099 <matteomania05@gmail.com>
2023-03-16 20:32:41 +0100

    Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <!-- navbar -->
   <header class="m-0 flex flex-wrap items-center justify-between px-2 py-3 bg-green-500 mb-3">
     <div ref="wrapper" @click="handleOutsideClick" class="container px-4 mx-auto flex flex-wrap items-center justify-between">
-      <div class="w-full relative flex justify-between ">
+      <div class="w-full fullata relative flex justify-between ">
         <Router-Link
           class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white outline-none focus:outline-none active:outline-1 active:outline active:outline-slate-300 active:rounded p-3"
           to="/">
@@ -11,15 +11,14 @@
           </div>
         </Router-Link>
         <button
-          class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none active:outline-1 active:outline active:outline-slate-300"
+          class="text-white hamburger cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none active:outline-1 active:outline active:outline-slate-300"
           type="button" ref="toggleButton" @click="isNavbarOpen = !isNavbarOpen">
           <i class="bi bi-list text-white text-2xl"></i>
         </button>
       </div>
     
-      <nav ref="navbar" v-show="isNavbarOpen"
-            class="flex w-full flex-col justify-center items-center">
-        <div class="lg:flex flex-grow w-full justify-center items-center transition-all">
+      <nav ref="navbar" v-show="isNavbarOpen" class="navbar flex w-full flex-col justify-center items-center">
+        <div class="contenitore lg:flex flex-grow w-full justify-center items-center transition-all">
           <ul class="flex flex-col list-none mx-auto">
             <li class="nav-item">
               <RouterLink
@@ -53,7 +52,6 @@
         </div>
       </nav>
     </div>
-
   </header>
 </template>
 
@@ -91,6 +89,18 @@ export default {
 </script>
 
 <style>
+@media screen and (min-width: 768px) {
+  .hamburger {
+    display: none !important;
+  }
+  .navbar, .navbar .contenitore, .fullata {
+    display: flex !important;
+    width: fit-content !important;
+  }
+  .navbar ul {
+    flex-direction: row !important;
+  }
+}
 .imageLogo {
   border-radius: 50%;
   width: 40px;
