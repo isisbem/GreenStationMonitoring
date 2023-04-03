34880fbd32efe9bd0d7d11e8b47751a810d199e8
Matteo0099 <matteomania05@gmail.com>
2023-04-03 18:35:22 +0200

    Some fix

--- src/views/Informations.vue
+++ src/views/Informations.vue
@@ -115,71 +115,62 @@ import Footer from '../components/Footer.vue'
                     </div>
                     <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:py-24 sm:px-28">
                         <!-- form (basin form)-->
-                        <form action="https://usebasin.com/f/37a6db30912f" 
-                            method="POST"
-                            enctype="multipart/form-data"
-                            id="form" 
-                            @submit.prevent="submitForm">
-                            <div class="max-w-full mx-auto">
-                                <h1 class="text-2xl font-semibold">Send a message!</h1>
-                                <div class="divide-y divide-gray-200">
-                                    <div class="py-8 sm:min-w-[375px] text-base leading-5 space-y-7 text-gray-700 sm:text-lg sm:leading-7">
-                                        <div class="relative">
-                                            <input v-model="email" @input="validateEmail" :class="{ 'border-red-500': !isValidEmail }" 
-                                                    autocomplete="off" required id="email" name="email" 
-                                                    type="text" placeholder="Email address"
-                                                    class="peer placeholder-transparent h-10 w-full 
-                                                        focus:border-b-green-500 border-b-2 border-gray-300
-                                                        text-gray-900 focus:outline-none focus:borer-rose-600
-                                                        focus:shadow-sm" />
-                                            <label 
-                                                for="email-address"
-                                                class="absolute left-0 -top-3.5 text-gray-600 text-sm 
-                                                          peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 
-                                                          peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5
-                                                          peer-focus:text-sm peer-focus:text-green-500 text-clip
-                                                          peer-focus:after:border-green-500 peer-disabled:text-transparent">
-                                                Email Address
-                                            </label>
-                                        </div>
-                                        <div class="relative">
-                                            <div class="relative w-full min-w-[200px]">
-                                                <textarea class="peer h-full min-h-[150px] w-full resize-none rounded-[7px] border 
-                                                          border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 
-                                                          font-sans text-sm font-normal text-blue-gray-700 outline outline-0 
-                                                          transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 
-                                                          placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-green-500 
-                                                          focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 
-                                                          disabled:bg-blue-gray-50 focus:shadow-md" 
-                                                          placeholder="" name="message" required></textarea>
-                                                <label
-                                                    class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 
-                                                          -top-1.5 flex h-full w-full select-none text-[13px] font-normal leading-tight 
-                                                          text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] 
-                                                          before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 
-                                                          before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 
-                                                          before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 
-                                                          after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow 
-                                                          after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 
-                                                          after:transition-all peer-placeholder-shown:text-sm
-                                                          peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 
-                                                          peer-placeholder-shown:before:border-transparent 
-                                                          peer-placeholder-shown:after:border-transparent peer-focus:text-[13px] 
-                                                          peer-focus:leading-tight peer-focus:text-green-500 peer-focus:before:border-t-2 
-                                                          peer-focus:before:border-l-2 peer-focus:before:border-green-500 
-                                                          peer-focus:after:border-t-2 peer-focus:after:border-r-2peer-focus:after:border-green-500" for="message">
-                                                    Message
-                                                </label>
-                                            </div>
-                                        </div>
-                                        <div class="relative">
-                                            <button type="submit" class="bg-green-500 hover:bg-green-600 text-white font-medium 
-                                                        py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline-green 
-                                                        active:bg-green-700 transition-all hover:-translate-y-1 hover:shadow-md">
-                                                Send Message
-                                            </button>
-                                        </div>
+                        <form action="https://usebasin.com/f/37a6db30912f" method="POST" enctype="multipart/form-data"
+                            id="form" @submit.prevent="submitForm">
+                            <div class="py-8 sm:min-w-[375px] text-base leading-5 space-y-7 text-gray-700 sm:text-lg sm:leading-7">
+                                <h1 class="font-bold py-2 mx-auto w-full text-xl sm:text-2xl">Send a message</h1>
+                                <div class="relative">
+                                    <input v-model="email" @input="validateEmail"
+                                        :class="{ 'border-red-500': !isValidEmail }" autocomplete="off" required
+                                        id="email" name="email" type="text" placeholder="Email address" class="peer placeholder-transparent h-10 w-full 
+                                                    focus:border-b-green-500 border-b-2 border-gray-300
+                                                    text-gray-900 focus:outline-none focus:borer-rose-600
+                                                    focus:shadow-sm" />
+                                    <label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm 
+                                                    peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 
+                                                    peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5
+                                                    peer-focus:text-gray-600 peer-focus:text-sm">
+                                        Email Address
+                                    </label>
+                                </div>
+                                <div class="relative">
+                                    <div class="relative w-full min-w-[200px]">
+                                        <textarea class="peer h-full min-h-[150px] w-full resize-none rounded-[7px] border 
+                                                      border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 
+                                                      font-sans text-sm font-normal text-blue-gray-700 outline outline-0 
+                                                      transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 
+                                                      placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-green-500 
+                                                      focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 
+                                                      disabled:bg-blue-gray-50 focus:shadow-md" placeholder=""
+                                            name="message" required></textarea>
+                                        <label
+                                            class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 
+                                                      -top-1.5 flex h-full w-full select-none text-[13px] font-normal leading-tight 
+                                                      text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] 
+                                                      before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 
+                                                      before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 
+                                                      before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 
+                                                      after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow 
+                                                      after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 
+                                                      after:transition-all peer-placeholder-shown:text-sm
+                                                      peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 
+                                                      peer-placeholder-shown:before:border-transparent 
+                                                      peer-placeholder-shown:after:border-transparent peer-focus:text-[13px] 
+                                                      peer-focus:leading-tight peer-focus:text-green-500 peer-focus:before:border-t-2 
+                                                      peer-focus:before:border-l-2 peer-focus:before:border-green-500 
+                                                      peer-focus:after:border-t-2 peer-focus:after:border-r-2peer-focus:after:border-green-500"
+                                            for="message">
+                                            Message
+                                        </label>
                                     </div>
+                                </div>
+                                <div class="relative">
+                                    <button type="submit"
+                                        class="bg-green-500 hover:bg-green-600 text-white font-medium 
+                                                    py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline-green 
+                                                    active:bg-green-700 transition-all hover:-translate-y-1 hover:shadow-md">
+                                        Send Message
+                                    </button>
                                 </div>
                             </div>
                         </form>
@@ -195,22 +186,22 @@ import Footer from '../components/Footer.vue'
 
 <script>
 export default {
-  data() {
-    return {
-      email: '',
-      isValidEmail: true
-    }
-  },
-  methods: {
-    validateEmail() {
-      this.isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)
+    data() {
+        return {
+            email: '',
+            isValidEmail: true
+        }
     },
-    submitForm() {
-      if (!this.isValidEmail) {
-        return
-      }
-      // submit form
+    methods: {
+        validateEmail() {
+            this.isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)
+        },
+        submitForm() {
+            if (!this.isValidEmail) {
+                return
+            }
+            // submit form
+        }
     }
-  }
 }
 </script>
