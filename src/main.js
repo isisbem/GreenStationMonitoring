7cd2372e2b306f293929d1523e2739942e5957b8
Matty <74145047+Matteo0099@users.noreply.github.com>
2023-04-20 15:53:57 +0200

    Installing node API dependencies

--- src/main.js
+++ src/main.js
@@ -5,24 +5,40 @@ import App from './App.vue'
 import './style.css'
 import './assets/main.css'
 
+//import servers
+// import { createServer } from './api/users/server.js'
+// Create and configure the server
+// const server = createServer()
+// server.listen(3080, () => {
+//   console.log(`Server is running on port 3080`)
+// })
+
 //import the charts
 import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 //gsap animation
-import { gsap } from "gsap";
-import { PixiPlugin } from "gsap/PixiPlugin.js";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
+import { gsap } from 'gsap'
+import { PixiPlugin } from 'gsap/PixiPlugin.js'
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin.js'
 //without this line, PixiPlugin and MotionPathPlugin may get dropped by your bundler (tree shaking)...
-gsap.registerPlugin(PixiPlugin, MotionPathPlugin);
+gsap.registerPlugin(PixiPlugin, MotionPathPlugin)
 
 //import aos animation
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 //starting aos
-render: (h) => h(App);
-AOS.init();
+render: h => h(App)
+AOS.init()
 
 //Create router
 import { createRouter, createWebHistory } from 'vue-router'
@@ -34,29 +50,29 @@ import Informations from './views/Informations.vue'
 
 //Get all pages
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'Home',
-            component: Home
-        },
-        {
-            path: '/about',
-            name: 'About',
-            component: About
-        },
-        {
-            path: '/Analytics',
-            name: 'Analytics',
-            component: Analytics
-        },
-        {
-            path: '/Informations',
-            name: 'Informations',
-            component: Informations
-        }
-    ]
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/Analytics',
+      name: 'Analytics',
+      component: Analytics
+    },
+    {
+      path: '/Informations',
+      name: 'Informations',
+      component: Informations
+    }
+  ]
 })
 
 //Create the app
