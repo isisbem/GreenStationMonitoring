ca5605df1af465feaa9213d5a07ef9a8866ccb97
Matty <74145047+Matteo0099@users.noreply.github.com>
2023-05-04 15:30:43 +0200

    fix

--- src/components/ChartsAnalytics.vue
+++ src/components/ChartsAnalytics.vue
@@ -1,15 +1,135 @@
 <template>
     <Bar ref="barChart" :bind="true" :data="chartData" />
-    <button @click="refresh" 
-            class="inline-flex justify-center items-center gap-1 bg-green-500 text-gray-50 uppercase 
+    <button @click="refresh" class="inline-flex justify-center items-center gap-1 bg-green-500 text-gray-50 uppercase 
                rounded-lg py-4 px-8 mt-6 hover:bg-green-600 transition
                hover:shadow-lg active:scale-95">
-               <i class="bi bi-arrow-clockwise text-xl md:text-3xl"></i>
-               <span>Refresh</span>
+        <i class="bi bi-arrow-clockwise text-xl md:text-3xl"></i>
+        <span>Refresh</span>
+    </button>
+</template>
+
+
+<script lang="ts">
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+// import { options } from '../js/Charts1'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+export default {
+    name: 'BarChart',
+    components: { Bar },
+    datasets: [{ label: 'Analytics' }],
+    data() {
+        return {
+            chartData: {
+                labels: [
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July",
+                    "August",
+                    "September",
+                    "October",
+                    "November",
+                    "December",
+                ],
+                datasets: [
+                    {
+                        label: "utilizzo",
+                        backgroundColor: "#f87979",
+                        data: [12, 20, 37, 39, 42, 40, 39, 30, 40, 20, 12, 11],
+                    },
+                ],
+            }
+        }
+    },
+    methods: {
+        refresh() {
+            const labels = [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+            ]
+            const data = [12, 20, 37, 39, 42, 40, 39, 30, 40, 20, 12, 11]
+            var i = 0;
+            for (i = 0; i <= 12; i++) {
+                this.chartData.labels[i] = labels[i];
+            }
+            // for(i = 0; i <= data.length; i++) {
+            //     this.chartData.datasets.data[i] = data[i];
+            // }
+        }
+    }
+}
+</script>
+
+<style>
+#my-chart-id {
+    width: 100%;
+    height: 100%;
+    image-rendering: optimizeQuality;
+}
+</style>
+
+<!-- <template>
+    <Bar ref="barChart" :bind="true" :data="chartData" />
+    <button @click="refresh" class="inline-flex justify-center items-center gap-1 bg-green-500 text-gray-50 uppercase 
+                 rounded-lg py-4 px-8 mt-6 hover:bg-green-600 transition
+                 hover:shadow-lg active:scale-95">
+        <i class="bi bi-arrow-clockwise text-xl md:text-3xl"></i>
+        <span>Refresh</span>
     </button>
 </template>
   
-<script lang="ts">
+<script>
+import { Bar } from 'vue-chartjs'
+import axios from 'axios'
+
+export default {
+    name: 'BarChart',
+    components: {
+        Bar
+    },
+    data() {
+        return {
+            chartData: {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'utilizzo',
+                        backgroundColor: '#f87979',
+                        data: []
+                    }
+                ]
+            }
+        }
+    },
+    mounted() {
+        this.refresh()
+    },
+    methods: {
+        async refresh() {
+            const { data } = await axios.get('https://82.223.8.163')
+            this.$set(this.chartData.labels, data.labels)
+            this.$set(this.chartData.datasets[0].data, data.values)
+            this.$refs.barChart.update()
+        }
+    }
+}
+</script> -->
+  
+<!-- <script lang="ts">
 import { Bar } from 'vue-chartjs';
 import axios from 'axios';
 import {
@@ -63,10 +183,9 @@ export default {
         async refresh() {
             try {
                 const response = await axios.get('http://82.223.8.163:22/your/api/path', {
-                    auth: {
-                        username: 'gsm',
-                        password: 'GsM!2023',
-                    },
+                    // auth: {
+                    //     //username:, //password: 
+                    // },
                 });
 
                 if (response && response.data) {
@@ -80,17 +199,7 @@ export default {
         },
     },
 };
-</script>
-  
-<style>
-#my-chart-id {
-    width: 100%;
-    height: 100%;
-    image-rendering: optimizeQuality;
-}
-</style>
-
-
+</script> -->
 
 <!-- EXAMPLE CODE FETCHING TO API... -->
 <!-- <template>
